e2e99495f645474ede52b042ee56b69b1b2e27b8
Russell Gordon <github@russellgordon.ca>
2022-05-03 09:33:17 -0400

    Initial Commit

---
+++ NoughtsAndCrosses/NoughtsAndCrossesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoughtsAndCrossesApp.swift
+//  NoughtsAndCrosses
+//
+//  Created by Russell Gordon on 2022-05-03.
+//
+
+import SwiftUI
+
+@main
+struct NoughtsAndCrossesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
